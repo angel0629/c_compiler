833ca13418e4c203aad62af51e264ac9c3151c77
angel0629 <angel99088@gmail.com>
2025-08-13 15:11:38 +0800

    合併後修改完成

--- main.c
+++ main.c
@@ -1,12 +1,7 @@
 #include <stdio.h>
 int main() {
-char name[100];
-printf("請輸入你的名字: ");
-scanf("%s", name);
-printf("你好, %s!", name);
-int age;
-printf("請輸入你的年齡: ");
-scanf("%d", &age);
-printf("age: %d!", age);
+for (int i=0; i<100; i++){
+    printf("%d\n",i);
+}
 return 0;
 }

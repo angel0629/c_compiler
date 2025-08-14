d702b4916910aefcde69e0db224ed691293b2651
Sean90332 <s111213078@mail1.ncnu.edu.tw>
2025-08-14 21:56:13 +0800

    不重要

--- main.c
+++ main.c
@@ -2,9 +2,9 @@
 #include<stdlib.h>
 
 int main(){
-    char ch[100];
-    //printf("your input:");
-    scanf("%s",ch);
-    printf("hello, %s",ch);
+    int a;
+    printf("輸入:");
+    scanf("%d",&a);
+    printf("你剛剛輸入:%d",a);
     return 0;
 }
